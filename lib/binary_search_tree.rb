3f40307037f72257883ba7f9cf95170748ca8d91
Taylor <taylor@crucialartists.com>
2023-03-24 15:24:52 -0500

    Complete assignment

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -8,11 +8,49 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+    return nil if @root.nil?
+
+    current = @root
+    found = false
+
+    while current && !found
+      if value < current.value
+        current = current.left
+      elsif value > current.value
+        current = current.right
+      else
+        found = true
+      end
+    end
+    found ? current : nil
   end
 
   def insert(value)
-    # your code here
+    new_node = Node.new(value)
+    if @root.nil?
+      @root = new_node
+      return @root
+    end
+
+    current = @root
+
+    while current
+      return nil if value == current.value
+
+      if value < current.value
+        if current.value.nil?
+          current.left = new_node
+          return current.left
+        end
+        current = current.left
+      else
+        if current.right.nil?
+          current.right = new_node
+          return current.right
+        end
+        current = current.right
+      end
+    end
   end
 
 end 
